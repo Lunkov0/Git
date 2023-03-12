a9237f817056b3e71b7cf280121f766d18c81316
Lunkov0 <89242194144@mail.ru>
2023-03-12 14:39:12 +1000

    Добавлена функция mylty (умножить)

--- git_learning.py
+++ git_learning.py
@@ -1,10 +1,15 @@
 def plus(a, b):
     return a + b
 
+
 def minus(a, b):
     return a - b
+
+
+def multy(a, b):
+    return a * b
+
 
 if __name__ == "__main__":
     print(plus(2, 2))
     print(minus(4, 2))
-
